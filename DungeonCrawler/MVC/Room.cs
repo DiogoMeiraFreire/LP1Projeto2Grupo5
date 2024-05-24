acb1028304a503a1adcba5e9656628f8cd8e039e
a22007432 <ssm.antonio@gmail.com>
2024-05-24 14:02:33 +0100

    Added Room class

---
+++ DungeonCrawler/MVC/Room.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler.MVC
+{
+    public class Room
+    {
+        // this class should handle the room logic
+        // Process room description.
+        //Room Logic
+    }
+}
